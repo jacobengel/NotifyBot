4e4a18610a7a906e92db9d8769f3726979a31899
Aleardi, Dominick <daleardi@5W5CN32.mi.corp.rockfin.com>
2016-01-22 02:23:40 -0500

    add email

--- NotifyBot/Controllers/NotifyController.cs
+++ NotifyBot/Controllers/NotifyController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 
 namespace NotifyBot.Controllers
 {
+    using System.Net.Mail;
+
     using NotifyBot.Models;
 
     public class NotifyController : ApiController
@@ -17,12 +19,22 @@ namespace NotifyBot.Controllers
             HttpResponseMessage responseMessage;
             try
             {
+                var senderName = request.Item.message.from.name;
+                var senderMention = request.Item.message.from.mention_name;
                 var text = request.Item.message.message.Substring(8);
+
+                
+                var to = "dominickaleardi@quickenloans.com";
+                var subject = senderName + " AKA " + senderMention + " has notified you!";
+                var body = text;
+
+                this.sendEmail(to, subject, body);
+
 
                 var message = new NotifyResponse
                 {
                     color = "green",
-                    message = "It's going to be sunny tomorrow! (" + text + ")",
+                    message = "It's going to be sunny tomorrow! (" + text + " )",
                     message_format = "text",
                     notify = "false"
                 };
@@ -33,6 +45,31 @@ namespace NotifyBot.Controllers
                 responseMessage = this.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
             }
             return responseMessage;
+        }
+
+        private void sendEmail(string to, string subject, string body)
+        {
+            var message = new MailMessage();
+            const string FromEmail = "hipchatemailbot@gmail.com";
+            const string FromPw = "winlikecharliesheen";
+            var toEmail = to;
+            message.From = new MailAddress(FromEmail);
+            message.To.Add(toEmail);
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = false;
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtpClient.EnableSsl = true;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(FromEmail, FromPw);
+
+                smtpClient.Send(message.From.ToString(), message.To.ToString(),
+                                message.Subject, message.Body);
+            }
         }
     }
 }
