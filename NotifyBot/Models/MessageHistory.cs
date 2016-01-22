b4855c4660a9923cc9e9ad5f611593f0b2f9bdf5
jengel <JacobEngel@quickenloans.com>
2016-01-22 09:03:48 -0500

    Adding logic to grab room history and render as HTML table.

---
+++ NotifyBot/Models/MessageHistory.cs
@@ -0,0 +1,37 @@
+﻿namespace NotifyBot.Models
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Web.UI;
+    using System.Web.UI.HtmlControls;
+
+    public class MessageHistory
+    {
+        public Dictionary<string, string> MessagesBySender { get; set; }
+
+        public string GetEmailMessage()
+        {
+            var table = new HtmlTable();
+
+            foreach (var message in this.MessagesBySender)
+            {
+                var row = new HtmlTableRow();
+
+                var fromCell = new HtmlTableCell { InnerText = message.Key };
+                var messageCell = new HtmlTableCell { InnerText = message.Value };
+
+                row.Cells.Add(fromCell);
+                row.Cells.Add(messageCell);
+
+                table.Rows.Add(row);
+            }
+
+            using (var writer = new StringWriter())
+            {
+                table.RenderControl(new HtmlTextWriter(writer));
+
+                return writer.ToString();
+            }
+        }
+    }
+}
---
+++ NotifyBot/Utility/RoomHistory.cs
@@ -0,0 +1,42 @@
+﻿namespace NotifyBot.Utility
+{
+    using System;
+    using System.Collections.Generic;
+
+    using HipchatApiV2;
+
+    using NotifyBot.Models;
+
+    public class RoomHistory
+    {
+        private readonly HipchatClient client;
+
+        private readonly string roomName;
+
+        public RoomHistory(string roomName)
+        {
+            this.client = new HipchatClient("9546c6c3c0b41f88abfda6bb6e0641");
+
+            this.roomName = roomName;
+        }
+
+        public MessageHistory GetRoomMessageHistory(int numPreviousMessages)
+        {
+            if (numPreviousMessages > 100)
+            {
+                throw new InvalidOperationException("Cannot get more than 100 messages of room history.");
+            }
+
+            var history = this.client.ViewRoomHistory(this.roomName, maxResults: numPreviousMessages);
+
+            var messageHistory = new MessageHistory { MessagesBySender = new Dictionary<string, string>() };
+
+            foreach (var record in history.Items)
+            {
+                messageHistory.MessagesBySender.Add(record.From, record.Message);
+            }
+
+            return messageHistory;
+        }
+    }
+}
