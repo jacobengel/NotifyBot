ef1f5d10e8b2f321519b7f720d0ed062481f0092
Aleardi, Dominick <daleardi@5W5CN32.mi.corp.rockfin.com>
2016-01-22 00:50:40 -0500

    initial commit

---
+++ NotifyBot/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NotifyBot.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ NotifyBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NotifyBot.WebApiApplication" Language="C#" %>
